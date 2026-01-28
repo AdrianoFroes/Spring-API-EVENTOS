90318f4740b23105977de11f85f748d1944ad000
adrianocosta <adrianocosta@CRONBK522.rotadooestesa.net>
2026-01-28 09:21:53 -0400

    commit1

---
+++ API_EVENTOS/src/main/java/br/com/eventoapp/repositoy/ConvidadoRepository.java
@@ -0,0 +1,12 @@
+package br.com.eventoapp.repositoy;
+
+import org.springframework.data.repository.CrudRepository;
+
+import br.com.eventoapp.models.Convidado;
+import br.com.eventoapp.models.Evento;
+
+
+public interface ConvidadoRepository extends CrudRepository<Convidado, String> {
+	Iterable<Convidado> findByEvento(Evento evento);
+
+}
